4ffa4b55e9aebcfcdb6326a3812b9a7983e94019
kermokao <kermo.kao@voco.ee>
2026-03-03 13:21:40 +0200

    Add Student class with initialization and method
    Implement a simple Student class with initialization and a method to check if the student has finished.

---
+++ oop_simple.py
@@ -0,0 +1,14 @@
+"""Simple class."""
+
+
+class Student:
+    """."""
+
+    def __init__(self, name, finished=False):
+        """."""
+        self.name = name
+        self.finished = finished
+
+    def is_finished(self):
+        """."""
+        return self.finished
